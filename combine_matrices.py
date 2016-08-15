b21bc3198246a57acdb06e0e46178667ee1c0aa7
k.doroschak@gmail.com <k.doroschak@gmail.com>
2016-08-15 10:04:10 -0700

    Fixed bug where I forgot to add the path prefix to the filename when being added to the dictionary list and not just creating a new entry. Also inserted a temporary workaround since the lists take awhile to generate in the first place

--- combine_matrices.py
+++ combine_matrices.py
@@ -13,7 +13,7 @@ def combine_matrices(indir, outdir):
 	if os.path.exists(outdir + "/unique_genes.npy") \
 	   and os.path.exists(outdir + "/unique_genes_matrix_filenames.npy"):
 	   print "Yay! The files exist so we don't have to read everything in again from scratch."
-	   genes = np.load(outdir + "/unique_genes.npy").item()
+	   genes = np.load(outdir + "/unique_genes.npy")
 	   gene_files = np.load(outdir + "/unique_genes_matrix_filenames.npy").item()
 	else:
 		print "Reading in matrix files (this will take awhile)..."
@@ -21,11 +21,11 @@ def combine_matrices(indir, outdir):
 		matrix_files = [i for i in matrix_files if os.path.isfile(indir + "/" + i) and not i.startswith(".") and "unique" not in i]
 		for matrix_file in matrix_files:
 			print matrix_file
-			gene = matrix_file.split("_")[-2]
+			gene = matrix_file.split("_")[-3]
 			print gene
 			genes.add(gene)
 			try:
-				gene_files[gene].append(matrix_file)
+				gene_files[gene].append(indir + "/" + matrix_file)
 			except:
 				gene_files[gene] = [indir + "/" + matrix_file]
 		np.save(outdir + "/unique_genes.npy", list(genes))
@@ -34,17 +34,24 @@ def combine_matrices(indir, outdir):
 	print "Processing each gene..."
 	for gene in genes:
 		files = gene_files[gene]
-		process_gene(gene, files, outdir)
+		process_gene(gene, files, outdir, indir=indir)
 	print "Done..."
 		
 
-def process_gene(gene_name, gene_matrix_full_filenames, outdir):
+def process_gene(gene_name, gene_matrix_full_filenames, outdir, indir=None):
 	norm_chunks = {gene_name: []}
 	matrices = {} 
 	print "Reading in chunks for gene " + gene_name + " and computing norm."
 	for matrix_file in gene_matrix_full_filenames:
 		chunk_number = matrix_file.split("_")[-1]
-		matrix = np.load(matrix_file)
+		try:
+			matrix = np.load(matrix_file)
+		except:
+			try:
+				matrix = np.load(indir + "/" + matrix_file)
+			except:
+				print matrix_file
+				return
 		#I know this structure doesn't make sense. Following what was done before
 		contact_probs = np.linalg.norm(matrix, axis=1)
 		norm_chunks[gene_name].append([chunk_number, None, contact_probs])
